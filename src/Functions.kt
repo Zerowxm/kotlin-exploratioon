a6634edebcb9b718dc7f1b0db975fa557bed407f
Zerowxm <zerowxm@gmail.com>
2018-02-19 23:28:44 -0500

    functions: undefined number of arguments

--- src/Functions.kt
+++ src/Functions.kt
@@ -4,8 +4,9 @@ fun returnNum(num : Int) : Int{
     return num
 }
 
-fun returnNothing() : Nothing{
-    throw Exception("There is nothing")
+fun returnNothing() : Int {
+    println("This is nothing")
+    return returnNum(1)
 }
 
 fun returnUnit() : Unit{
@@ -17,10 +18,22 @@ fun overload(x : Int,y : Int,z :Int = 0,w : Int = 0) = print(x + y + z + w)
 fun defaultArguments(name : String, email : String = "", phone : String){
     println("I am $name, email:$email, phone:$phone")
 }
+
+fun printStrings(vararg strings: String){
+    reallyPrintStrings(*strings)
+}
+
+private fun reallyPrintStrings(vararg strings: String) {
+    for (string in strings) {
+        println(string)
+    }
+}
+
 fun main(args: Array<String>) {
     overload(1,2)
     returnNothing()
     returnNum(1)
     defaultArguments("Xiaoming",phone = "110")
     defaultArguments("Rem",phone = "120", email = "NA")
+    printStrings("1","2","3")
 }
